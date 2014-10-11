38a9ad2b5897740086fe07fad43ce2f9dd8fae1d
John Shiver <johneshiver@gmail.com>
2014-10-10 23:12:39 -0700

    Added cities

--- cidh/cities/admin.py
+++ cidh/cities/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import City
 
 
-admin.site.register(City)
+class CityAdmin(admin.ModelAdmin):
+
+    fields = ('name',
+              'country',
+              'is_legal')
+
+
+admin.site.register(City, CityAdmin)
--- cidh/cities/models.py
+++ cidh/cities/models.py
@@ -7,3 +7,6 @@ class City(models.Model):
     name = models.CharField(max_length=50)
     country = models.CharField(max_length=10)
     drink_legal = models.BooleanField()
+
+    def __str__(self):
+        return '%s, %s' % (self.name, self.country)
