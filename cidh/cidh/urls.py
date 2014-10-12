42b0d54897c6ac2afffff4193d5de775d8017995
John Shiver <johneshiver@gmail.com>
2014-10-12 12:49:55 -0700

    Enabled geolocation + fixed url views

--- cidh/cidh/urls.py
+++ cidh/cidh/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', Home.as_view(), name='home'),
     url(r'^city/(?P<city_id>\d+)/$', cityView.as_view(), name='city_view'),
-    url(r'^city_detail/(?P<city_id>\d+)/$', cityDetailView.as_view(), name='city_view'),
-    url(r'^list/', listView.as_view(), name='list'),
+    url(r'^city_detail/(?P<city_id>\d+)/$', cityDetailView.as_view(), name='city_detail'),
+    url(r'^list/', listView.as_view(), name='list_view'),
     # url(r'^disclaimer/', disclaimerView.as_view()),
 )
 
